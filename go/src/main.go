4cc1c286084a378c75343bea9c207837a46df75e
David Butler <dbutler4@hawk.iit.edu>
2019-09-27 10:50:34 -0500

    Can interface with a yaml file, created yaml with example data

--- go/src/main.go
+++ go/src/main.go
@@ -4,11 +4,46 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"path/filepath"
 )
 
+type Config struct{
+	Test string `yaml:"Test"`
+	Rentals []Rental `yaml:"checkedOut"`
+} 
+
+
+type Rental struct{
+	Tag string `yaml:"tag,omitempty"`
+	Drinker string `yaml:"drinker,omitempty"`
+	Location string `yaml:"location,omitempty"`
+	Date string `yaml:"date,omitempty"`
+	Card string `yaml:"card,omitempty"`
+}
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Koffee Member Name: ")
+	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
-	fmt.Println("What's good, " + text+"?")
-	 }
+	fmt.Println("What's good, " + text + "?")
+
+	filename, _ := filepath.Abs("/home/david/Development/KoffeeCupLogger/tracking/testprof.yaml")
+	fmt.Println("Directory: " + filename)
+	yamlFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("ERROR 1")
+		panic(err)
+	}
+
+	var config Config
+
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		fmt.Println("ERROR 2")
+		panic(err)
+	}
+
+	fmt.Printf("\n%#v\n", config)
+}
